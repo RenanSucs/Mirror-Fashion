1cecb31d520e1ca7ddbf7507a4b137e6bee25b5e
RenanSucs <47055395+RenanSucs@users.noreply.github.com>
2019-02-03 14:13:43 -0200

    Mirror Fashion finalizada

--- js/home.js
+++ js/home.js
@@ -2,4 +2,8 @@ $('.novidades button').click(function() {
     $('.novidades').toggleClass('painel-aberto')
 })
 
+$('.mais-vendidos button').click(function() {
+    $('.mais-vendidos').toggleClass('painel-aberto')
+})
+
 
--- js/produto.js
+++ js/produto.js
@@ -8,3 +8,4 @@ function mostraTamanho(){
 inputTamanho.oninput = mostraTamanho
 
 mostraTamanho()
+
